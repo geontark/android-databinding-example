285e586cfdf7cf93e4eeb463f271911a6d715526
geontark <geontark0623@gmail.com>
2020-10-21 20:52:11 +0900

    first commit

--- app/src/main/java/com/test/myapplication/MainVM.kt
+++ app/src/main/java/com/test/myapplication/MainVM.kt
@@ -1,13 +1,12 @@
 package com.test.myapplication
 
 import android.text.Editable
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 
 class MainVM {
-    val _oneWay = MutableLiveData<String>()
+    private val _oneWay = MutableLiveData<String>()
     val oneWay: LiveData<String> = _oneWay
 
     val twoWay = MutableLiveData<String>()
@@ -22,5 +21,4 @@ class MainVM {
     fun oneWayF(s: Editable) {
         if (_oneWay.value != s.toString()) _oneWay.postValue(s.toString())
     }
-
 }
